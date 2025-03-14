a3199b4a6558fa5ee395c9c8323ec1f9219d6e5b
Nan-Hung Hsieh <nan_1003@hotmail.com>
2025-03-14 12:09:07 -0700

    1_descriptive

--- 1_descriptive.R
+++ 1_descriptive.R
@@ -1,212 +1,145 @@
-
-library(readxl) # read_excel
+library(readxl)  # read_excel
 library(foreign) # read.xport
-library(survey) # svydesign
+library(survey)  # svydesign
+library(dplyr)   # For efficient data manipulation
+library(skimr)   # For detailed summary statistics
 
-# Process each cohort
-
+# Load NHANES codes from NHANES Excel file
 codes_file <- "data/NHANEScodes_file.xlsx"
-NHANEScodes <- codes_file
-wtvars <- as.data.frame(read_excel(NHANEScodes, sheet = 2))
-demofiles <- wtvars$demofile
-datafiles <- wtvars$file
-bwtfiles <- wtvars$BWfile
-creatfiles <- wtvars$creatfile
-wtvar <- wtvars$wtvariable
-names(demofiles) <- names(datafiles) <-
-  names(bwtfiles) <- names(creatfiles) <- 
-  names(wtvar) <- wtvars$sample
-
+if (!file.exists(codes_file)) stop("Error: 'codes_file' not found at ", codes_file)
+wtvars <- read_excel(codes_file, sheet = 2) |> as.data.frame()
+demofiles <- setNames(wtvars$demofile, wtvars$sample)
+datafiles <- setNames(wtvars$file, wtvars$sample)
+bwtfiles <- setNames(wtvars$BWfile, wtvars$sample)
+creatfiles <- setNames(wtvars$creatfile, wtvars$sample)
 
 # Define cohort and year
 year <- c("2015")
 cohort <- c("15-16")
-datafiles <- c("uphopm_i.xpt")
-wtvariable  <- c("WTSB2YR")
+datafiles <- c("ssneon_i.xpt")
+chem2yrwt  <- c("WTSSBI2Y")
 
+# Load pre-trained models for creatinine calculation
+for (file in c("Wtns.rda", "Agens.rda", "modparms.rda")) {
+  if (!file.exists(file.path("data", file))) stop("Error: Missing ", file)
+}
+load("data/Wtns.rda")
+load("data/Agens.rda")
+load("data/modparms.rda")
 
-# Process each cohort
+# Define creatinine calculation function
+# Define creatinine calculation function
+CreatFun <- \(newdata) {
+  X <- cbind(model.matrix(~0 + RIAGENDR + RaceEthn, data = newdata),
+             predict(Wtns, newdata$BMXWT),
+             predict(Agens, newdata$RIDAGEYR))
+  10^(X %*% modparms[-length(modparms)])
+}
+
+# Define required chemical variables
+chemvars <- c("SSIMID", "SSACET", "SSCLOT", "SSTHIA", "SSOHIM", "SSAND")
+LODnames <- c("SSIMIDLC", "SSACETLC", "SSCLOTLC", "SSTHIALC", "SSOHIMLC", "SSANDLC")
+
+# Define required demographic variables
+seq <- "SEQN"
+PSU <- "SDMVPSU"          # Sampling unit
+STRA <- "SDMVSTRA"        # Stratum for each observation
+demoageyr <- "RIDAGEYR"   # Age in years
+demogendr <- "RIAGENDR"   # Gender
+demoeth <- "RIDRETH1"     # Ethnicity
+MECwt <- "WTMEC2YR"       # MEC weight
+bodywtcomment <- "BMIWT"  #
+creatinine <- "URXUCR"    # Urinary creatinine
+bodywt <- "BMXWT"         #
+bodymassindex <- "BMXBMI" # 
+
+# Process each cohort (single iteration for "15-16")
 for (j in 1) {
-  select_cohort <- cohort[j]
-  locat <- paste0("data/nhanes/", year[j])
-  demo_locat <- paste0(
-    locat, "/", unique(demofiles[which(names(demofiles) == cohort[j])])
-  )
-  data_locat <- paste0(locat, "/", datafiles[j])
-  bwt_locat <- paste0(
-    locat, "/", tolower(unique(bwtfiles[which(names(bwtfiles) == cohort[j])]))
-  )
-  creat_locat <- paste0(
-    locat, "/",unique(creatfiles[which(names(creatfiles) == cohort[j])])
-  )
+  
+  # Read NHANES data
+  select_cohort <- cohort
+  locat <- file.path("data/nhanes", year)
+  demo_locat <- file.path(locat, unique(demofiles[names(demofiles) == cohort]))
+  data_locat <- file.path(locat, datafiles)
+  bwt_locat <- file.path(locat, tolower(unique(bwtfiles[names(bwtfiles) == cohort])))
+  creat_locat <- file.path(locat, unique(creatfiles[names(creatfiles) == cohort]))
+
+  # Read data with error checking
+  for (file in c(demo_locat, data_locat, bwt_locat, creat_locat)) {
+    if (!file.exists(file)) stop("Error: File not found: ", file)
+  }
   demo <- read.xport(demo_locat)
   cdta <- read.xport(data_locat)
   bwt <- read.xport(bwt_locat)
   creat <- read.xport(creat_locat)
-   chem2yrwt <- wtvariable[j]
-  met <- c("URX4FP", "URXOPM", "URXTCC", "URXCB3", "URXCCC")
-  if (all(met %in% names(cdta) == TRUE)) {
-    chemvars <- c("URX4FP", "URXOPM", "URXTCC", "URXCB3", "URXCCC")
-    LODnames <- c("URD4FPLC", "URDCB3LC", "URDCCCLC", "URDOPMLC", "URDTCCLC")
-  } else if (all(met[1:4] %in% names(cdta) == TRUE)){
-    chemvars <- c("URX4FP", "URXOPM", "URXTCC", "URXCB3")
-    LODnames <- c("URD4FPLC", "URDCB3LC", "URDOPMLC", "URDTCCLC")
-  } else {
-    chemvars <- c("URX4FP", "URXOPM", "URXTCC") 
-    LODnames <- c("URD4FPLC", "URDOPMLC", "URDTCCLC")
-  }
-  seq <- "SEQN"
-  PSU <- "SDMVPSU" # the sampling unit
-  STRA <- "SDMVSTRA" # the stratum for each observation
-  demoageyr <- "RIDAGEYR"
-  demogendr <- "RIAGENDR"
-  demoeth <- "RIDRETH1"
-  MECwt <- "WTMEC2YR"
-  bodywtcomment <- "BMIWT"
-  demo <- demo[,c(seq,PSU,STRA,demoageyr,demogendr,demoeth,MECwt)]
-  # Set up gender, age, and ethnicity as factors 
-  # using the same levels as the NHANES reports
-  demo[,demogendr] <- factor(demo[,demogendr], labels=c("Male","Female"))
-  demo$AgeGroup <- cut(
-    demo[,demoageyr], 
-    breaks=c(-1,5.5,11.5,19.5,65.5, 100.5),
-    labels=c("0 - 5", "6 - 11", "12 - 19", "20 - 65", "Over 65")
-  )
-  demo$RaceEthn <- factor(
-    demo[,demoeth],
-    labels=c(
-      "Mexican American",
-      "Other Hispanic",
-      "Non-Hispanic White",
-      "Non-Hispanic Black","Other")
-  )
-  demo$ChildBearingAgeFemale <- factor(
-    demo[,demogendr] == "Female" & (demo[,demoageyr] >= 16 & demo[,demoageyr] <= 49),
-    labels=c("NotReproAgeFemale","ReproAgeFemale")
-  )
-  measurehead="URX"
-  measuretail=NULL
-  lodindhd="URD"
-  lodtail="LC"
-  meascore <- sub(
-    paste("^",measurehead,"(.+)",measuretail,"$",sep=""),"\\1",chemvars
-  )
-  LODnames <- paste(lodindhd,meascore,lodtail,sep="")
-  ## nms is a data frame with a record for each chemical.  It keeps
-  ## track of the names of the measurement and LODind variables, the
-  ## LOD, and space for the chemical name and CAS. LOD here is the
-  ## maximum of the individual level LODs.
-  nms <- data.frame(
-    Measurement=chemvars, 
-    LODind=LODnames, 
-    LOD=numeric(length(chemvars)),
-    Chem = character(length(chemvars)), 
-    CAS = character(length(chemvars)),
-    stringsAsFactors=FALSE
-  )
-  for (i in seq_len(nrow(nms))) {
-    nm <- nms$Measurement[i]
-    nmlc <- nms$LODind[i]
-    ## Do we already have an LOD indicator?
-    if (!nmlc %in% colnames(cdta)) {
-      ## We have to create the LOD indicator.  
-      ## In this case, there is only 1 LOD
-      ## and it will be sqrt(2) times the smallest value
-      zlod <- sort(unique(cdta[,nm]))[1] * sqrt(2)
-      cdta[,LODnames[i]] <- ifelse(cdta[,nm] >= zlod, 0, 1)
-    }
-  }
-  codes_file <- "data/NHANEScodes_file.xlsx"
-  creatinine <- "URXUCR"
+
+  # Process demographic data
+  demo <- demo |> 
+    select(SEQN, SDMVPSU, SDMVSTRA, RIDAGEYR, RIAGENDR, RIDRETH1, WTMEC2YR) |>
+    mutate(
+      RIAGENDR = factor(RIAGENDR, labels = c("Male", "Female")),
+      AgeGroup = cut(RIDAGEYR, breaks = c(-1, 5.5, 11.5, 19.5, 65.5, 100.5), 
+                     labels = c("0-5", "6-11", "12-19", "20-65", "Over 65")),
+      RaceEthn = factor(RIDRETH1, labels = c("Mexican American", "Other Hispanic", 
+                                             "Non-Hispanic White", "Non-Hispanic Black", "Other")),
+      ChildBearingAgeFemale = factor(RIAGENDR == "Female" & RIDAGEYR >= 16 & RIDAGEYR <= 49,
+                                     labels = c("NotReproAgeFemale", "ReproAgeFemale"))
+    )
+  # Merge datasets
   alldata <- merge(demo, cdta[,c(seq, chem2yrwt, chemvars, LODnames)],
     by.x=seq, by.y=seq, all.y=TRUE)
   alldata <- merge(alldata, creat[,c(seq, creatinine)],
     by.x=seq, by.y=seq, all.x=TRUE) # 2125
-  bodywt <- "BMXWT"
-  bodymassindex <- "BMXBMI"
-  bwt[!is.na(bwt[,bodywtcomment]),bodywt] <- NA
+   bwt[!is.na(bwt[,bodywtcomment]),bodywt] <- NA
+  
   ## Create obesity factor from bodymassindex
   bwt$Obesity <- cut(bwt[,bodymassindex], breaks=c(-0.5,30,500),
     labels=c("BMI <= 30", "BMI > 30"))
   alldata <- merge(alldata, bwt[,c(seq, bodywt, bodymassindex, "Obesity")],
     by.x=seq, by.y=seq, all.x=TRUE)
-  if (any(is.na(alldata[,bodywt]))) {
-    ##writeLines(paste(sum(is.na(alldata[,bodywt])),"missing values in",bodywt))
-    ##browser()
-    dta <- merge(
-      demo, bwt[,c(seq, bodywt)], by.x=seq, by.y=seq, all.x=TRUE, all.y=TRUE)
-    dsg <- na.omit(svydesign(ids=make.formula(PSU), strata=make.formula(STRA),
-        weights=make.formula(MECwt), nest=TRUE, data=dta))
-    ## Just fit separate means by age and gender
-    selectmales <- dsg$variables[,demogendr] == "Male"
-    selectfemales <- dsg$variables[,demogendr] == "Female"
-    males <- svyby(make.formula(bodywt), by=make.formula(demoageyr),
-      subset(dsg, selectmales), svymean)
-    females <- svyby(make.formula(bodywt), by=make.formula(demoageyr),
-      subset(dsg, selectfemales), svymean)
-    imp <- data.matrix(cbind(males[,bodywt],females[,bodywt]))
-    ## Now, do the imputation
-    ##browser()
-    isnabw <- is.na(alldata[,bodywt])
-    alldata[isnabw,bodywt] <- imp[
-      data.matrix(alldata[isnabw,c(demoageyr, demogendr)])
-      ]
-    ##writeLines(">>> Imputed bodyweights")
-    ##print(alldata[isnabw, c(demoageyr, demogendr, bodywt)])
+
+  # Impute missing body weights
+  if (any(is.na(alldata[[bodywt]]))) {
+    dta <- merge(demo, bwt[, c(seq, bodywt)], by.x = seq, by.y = seq, all.x = TRUE, all.y = TRUE)
+    dsg <- na.omit(svydesign(ids = as.formula(paste0("~", PSU)), 
+                             strata = as.formula(paste0("~", STRA)),
+                             weights = as.formula(paste0("~", MECwt)), 
+                             nest = TRUE, data = dta))
+    selectmales <- dsg$variables[[demogendr]] == "Male"
+    selectfemales <- dsg$variables[[demogendr]] == "Female"
+    males <- svyby(as.formula(paste0("~", bodywt)), as.formula(paste0("~", demoageyr)),
+                   subset(dsg, selectmales), svymean)
+    females <- svyby(as.formula(paste0("~", bodywt)), as.formula(paste0("~", demoageyr)),
+                     subset(dsg, selectfemales), svymean)
+    imp <- data.matrix(cbind(males[[bodywt]], females[[bodywt]]))
+    isnabw <- is.na(alldata[[bodywt]])
+    alldata[isnabw, bodywt] <- imp[cbind(alldata[isnabw, demoageyr], 
+                                         as.integer(alldata[isnabw, demogendr]))]
   }
+
   ## Fixup factors so there are no missing levels
   for (nm in names(alldata)) {
     if (is.factor(alldata[,nm])) {
       alldata[,nm] <- factor(alldata[,nm])}
   }
-  #
-  CreatFun <- function(newdata){
-    X <- cbind(model.matrix(~ 0 + RIAGENDR + RIDRETH1, data=newdata),
-      predict(Wtns, newdata$BMXWT),
-      predict(Agens, newdata$RIDAGEYR))
-    return(10^(X %*% modparms[-length(modparms)]))
-  }
-  #
-  load("data/Wtns.rda")
-  load("data/Agens.rda")
-  load("data/modparms.rda")
-  alldata$DailyCreatinine <-
-    CreatFun(data.frame(RIAGENDR = unname(alldata[,demogendr]),
-        RIDRETH1 = unname(alldata[,"RaceEthn"]),
-        BMXWT = unname(alldata[,bodywt]),
-        RIDAGEYR = unname(alldata[,demoageyr])))
-  #
-  LD_URXOPM <- min(alldata$URXOPM, na.rm = T)
-  alldata$LD_URXOPM <- min(alldata$URXOPM, na.rm = T)
-  LD_URXTCC <- min(alldata$URXTCC, na.rm = T)
-  alldata$LD_URXTCC <- min(alldata$URXTCC, na.rm = T)
-  LD_URX4FP <- min(alldata$URX4FP, na.rm = T)
-  alldata$LD_URX4FP <- min(alldata$URX4FP, na.rm = T)
-  if (all(met %in% names(cdta) == TRUE)){
-    LD_URXCB3 <- min(alldata$URXCB3, na.rm = T)
-    alldata$LD_URXCB3 <- min(alldata$URXCB3, na.rm = T)
-    LD_URXCCC <- min(alldata$URXCCC, na.rm = T)
-    alldata$LD_URXCCC <- min(alldata$URXCCC, na.rm = T)
-  } else if (all(met[1:4] %in% names(cdta) == TRUE)) {
-    LD_URXCB3 <- min(alldata$URXCB3, na.rm = T) 
-    alldata$LD_URXCB3 <- min(alldata$URXCB3, na.rm = T)
-  }
-  # tidy
-  if (length(which(is.na(alldata$URXUCR))) > 0) {
-    alldata <- alldata[-which(is.na(alldata$URXUCR)), ]
-  }
-  if (length(which(is.na(alldata$DailyCreatinine))) > 0) {
-    alldata <- alldata[-which(is.na(alldata$DailyCreatinine)), ]
-  }
-  if (length(which(is.na(alldata$BMXWT))) > 0) {
-    alldata <- alldata[-which(is.na(alldata$BMXWT)), ]
-  }
-  alldata$cohort <- cohort[j]
-  to_select <- c("AgeGroup", "BMXWT", "DailyCreatinine", "URXUCR", 
-    "URX4FP", "URXOPM", "URXTCC",
-    "LD_URX4FP", "LD_URXOPM", "LD_URXTCC", "cohort")
-  alldata <- alldata[, to_select] 
+    
+  # Calculate daily creatinine
+  alldata$DailyCreatinine <- CreatFun(alldata)
+  
+ # Clean and add cohort (updated for dplyr 1.1.0+)
+  alldata <- alldata |> 
+    filter_at(vars(URXUCR, DailyCreatinine, BMXWT), all_vars(!is.na(.))) |> 
+    mutate(cohort = cohort)
+
   if (j == 1) all_data <- alldata else all_data <- rbind(all_data, alldata)
 }
-alldata |> head()
 
+# Display first few rows
+message("\nFirst Few Rows of Processed Data:")
+head(all_data)
+
+# Descriptive Statistics
+message("Unweighted Descriptive Statistics:")
+skim(all_data)
+
+# Compare with Ospina et al. (2019)
